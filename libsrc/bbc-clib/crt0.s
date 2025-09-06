2ee73230ffd560f32e48c096da0c8280858e1089
Mark Fisher <mark.j.fisher@gmail.com>
2025-09-06 13:02:32 +0100

    Align bbc target with bbc-clib

--- libsrc/bbc-clib/crt0.s
+++ libsrc/bbc-clib/crt0.s
@@ -1,36 +1,35 @@
 ;
-; Startup code for cc65 BBC CLIB ROM target
-; REQUIRES cc65 CLIB ROM to be present in sideways slot 1
-;
+; Startup code for cc65 (bbc clib rom version)
+; REQUIRES cc65 CLIB ROM to be present in any sideways ROM
 
         .export         _exit
         .export         __STARTUP__ : absolute = 1      ; Mark as startup
+        .export         __Cstart
+        .export         _exit_main
 
         .import         initlib, donelib
         .import         zerobss
-        .import         callmain        
+        .import         callmain
         .import         preservezp, restorezp
         .import         _raise
 
         .import         disable_cursor_edit
         .import         restore_cursor_edit
         .import         init_stack
+
+        .import         OSWRCH
+        .import         cursor
+        .import         setcursor
+
+        .import         _install_brk_handler_global
+        .import         _uninstall_brk_handler_global
+
         .import         detect_clib_rom
         .import         clib_rom_available
         .import         clib_rom_slot
         .import         original_romsel
         .import         print_error_and_exit
         .import         rom_error_msg
-        .import         OSWRCH
-        .import         cursor
-        .import         setcursor
-        .import         _install_brk_handler_global
-        .import         _uninstall_brk_handler_global
-
-        ; .import         trap_brk, release_brk
-
-        .export         __Cstart
-        .export         _exit_main
 
         .include        "zeropage.inc"
         .include        "oslib/os.inc"
@@ -43,29 +42,21 @@ ROMSEL          := $FE30
 __Cstart:
 
 reset:
-
         ; Zero BSS segment BEFORE ROM detection
         ; (so we don't zero out clib_rom_available after setting it)
         jsr     zerobss
 
-        ; Check for cc65 CLIB ROM
         jsr     detect_clib_rom
-
-        ; ROM must be present - exit with error if not found
         lda     clib_rom_available
         bne     rom_found
 
-        ; ROM not found - display error and exit
         lda     #<rom_error_msg
         ldx     #>rom_error_msg
         jsr     print_error_and_exit
-        ; After error message, return to the user
-        ; debatable if we care about the stack pointer value restoration
         rts
 
 rom_found:
         jsr     disable_cursor_edit
-        ; setup c_sp for software stack pointer
         jsr     init_stack
 
         ; disable interrupts while we setup the vectors
@@ -100,11 +91,10 @@ rom_found:
 
         jsr     callmain
 
-_exit_main:     ; AX contains exit code, store LSB in user flag
-
+_exit_main:
         ; Save the exit code in user flag
-        tax                     ; Move to X for OSBYTE set user flag.
-        ldy     #255
+        tax
+        ldy     #$FF
         lda     #osbyte_USER_FLAG
         jsr     OSBYTE
 
@@ -122,6 +112,7 @@ _exit_main:     ; AX contains exit code, store LSB in user flag
         sei
         jsr     _uninstall_brk_handler_global
 
+        ; restore event handler
         lda     oldeventv
         sta     EVNTV
         lda     oldeventv+1
@@ -137,12 +128,14 @@ _exit_main:     ; AX contains exit code, store LSB in user flag
         lda     original_romsel
         sta     ROMSEL_CURRENT
         sta     ROMSEL
+
         jsr     _cleanup_display
 
 exit:
         rts
 
-_exit:  ldx     save_s                ; force return to OS
+; force return to OS, restoring SP will remove any potential return calls no longer needed
+_exit:  ldx     save_s
         txs
         jmp     _exit_main
 
@@ -157,18 +150,15 @@ eschandler:
         tya
         pha
 
-        ; preserve zp
         jsr     preservezp
-
-        cli                        ; reenable interrupts
+        cli
 
         ldx     #0
         lda     #3              ; SIGINT ???
         jsr     _raise
 
-        sei                        ; disable interrupts, as we pass it on...
-
-        ; restore zp
+        ; disable interrupts, as we pass it on...
+        sei
         jsr     restorezp
 
         pla
@@ -181,6 +171,7 @@ eschandler:
 
 nohandle:
         plp
+
         pha
         lda     oldeventv
         sta     @evj+1
@@ -191,7 +182,8 @@ nohandle:
 
 
 _cleanup_display:
-        lda     #$01    ; turn cursor back on, in case anything turned it off (e.g. cgetc() with default cursor value off)
+        ; turn cursor back on, in case anything turned it off (e.g. cgetc() with default cursor value off)
+        lda     #$01
         sta     cursor
         jsr     setcursor
         jsr     restore_cursor_edit
@@ -205,6 +197,6 @@ _cleanup_display:
         rts
 
         .bss
-oldeventv:         .res     2
-oldescen:          .res     1 
-save_s:            .res     1
+oldeventv:      .res    2
+oldescen:       .res    1       ; was escape event enabled before?
+save_s:	        .res    1       ; save stack pointer
